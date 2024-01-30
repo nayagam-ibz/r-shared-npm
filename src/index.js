5c7816a4cef2ca4bb8bfe34a795b5dae3800d1f5
Saravanan Pandiaraj <saravanan.pandiaraj@bitcontroltech.com>
2024-01-30 13:05:27 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
+import styles from './styles/Button.scss';
+import Button from 'react-bootstrap/Button';
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+  return (
+    <div>
+      <Button className={`${styles['my-button']} ${styles['disabled']}`}>
+        <div className="testing">This is a child divdddddddddddddd</div>
+      </Button>
+      <Button variant="primary">Primary</Button>
+
+    </div>
+  );
+};
