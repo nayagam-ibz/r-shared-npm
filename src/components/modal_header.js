151fd25245dde3957539880cf5c777468f292968
Saravanan Pandiaraj <saravanan.pandiaraj@bitcontroltech.com>
2024-01-30 16:53:42 +0530

    remove image

--- src/components/modal_header.js
+++ src/components/modal_header.js
@@ -4,12 +4,6 @@ function ModalHeader({ title, handleClose }) {
   return (
     <div className='modal_header px-4'>
       <h6 className='modal-title'>{title}</h6>
-      <img
-        src={require(`../src/images/cancel.png`)}
-        alt='cancel'
-        onClick={handleClose}
-        className='cursor-pointer'
-      />
     </div>
   )
 }
--- src/components/no_record.js
+++ src/components/no_record.js
@@ -6,11 +6,6 @@ function NoRecord({ colSpan }) {
       <td colSpan={colSpan}>
         <div className='d-flex align-items-center justify-content-center'>
           <div className='text-center py-5'>
-            <img
-              src={require(`../src/images/no-record.png`)}
-              alt='no_record'
-              width='100'
-            />
             <p className='mb-0'>No Record Found</p>
           </div>
         </div>
